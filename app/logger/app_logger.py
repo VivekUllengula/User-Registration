3bf2f299efacaf9a9b81d9f0bf3869cdd83b7b69
VivekUllengula <vivek.ullengula@thezennialpro.com>
2025-07-24 17:32:45 +0530

    Added the basic logging logic and db connectivity logic

---
+++ app/logger/app_logger.py
@@ -0,0 +1,9 @@
+import logging
+
+logging.basicConfig(
+    format="%(asctime)s - %(levelname)s - %(messages)s",
+    handlers=[logging.FileHandler("app.log"), logging.StreamHandler()],
+    level=logging.INFO
+)
+
+logger = logging.getLogger(__name__)
